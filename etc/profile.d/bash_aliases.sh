1bb6145169a26fe7b1ba2f47877896301d4b9c3a
Mario Villasante • GECI <42744723+mvillasante@users.noreply.github.com>
2023-07-13 12:51:17 -0600

    💫Update build-dev alias

--- etc/profile.d/bash_aliases.sh
+++ etc/profile.d/bash_aliases.sh
@@ -1,4 +1,4 @@
-alias build-dev='echo "FROM islasgeci/pde:latest" > ./Dockerfile.dev && tail --lines=+2 ./Dockerfile >> ./Dockerfile.dev && docker build --file ./Dockerfile.dev --tag islasgeci/${PWD##*/}:develop .'
+alias build-dev='cp Dockerfile Dockerfile.dev && curl https://raw.githubusercontent.com/IslasGECI/pde/develop/Dockerfile | tail --lines=+2 >> Dockerfile.dev && docker build --file ./Dockerfile.dev --tag islasgeci/${PWD##*/}:develop .'
 alias develop='nix develop "github:IslasGECI/pde"'
 alias lsp-nvim='docker run --interactive --rm --tty --volume ${PWD}:/workdir islasgeci/pde:latest'
 alias run-dev='docker run --env BITBUCKET_USERNAME --env BITBUCKET_PASSWORD --interactive --name ${PWD##*/}_dev --rm --tty --volume ${PWD}:/workdir islasgeci/${PWD##*/}:develop bash'
