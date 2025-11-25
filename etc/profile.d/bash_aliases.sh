ba583fd0e93bd6544dbc98ed4227c9f6d4b77b3a
Mario Villasante • GECI <42744723+mvillasante@users.noreply.github.com>
2025-11-25 16:37:40 -0600

    Fix open-r alias to use correct flags

--- etc/profile.d/bash_aliases.sh
+++ etc/profile.d/bash_aliases.sh
@@ -1,6 +1,6 @@
 alias build-dev='cp Dockerfile Dockerfile.dev && curl https://raw.githubusercontent.com/IslasGECI/pde/develop/Dockerfile | tail --lines=+2 >> Dockerfile.dev && docker build --file ./Dockerfile.dev --tag islasgeci/${PWD##*/}:develop .'
 alias lsp-nvim='docker run --interactive --rm --tty --volume ${PWD}:/workdir islasgeci/pde:latest'
-alias open-r="docker run -ivt $PWD:/workdir islasgeci/base R"
+alias open-r="docker run -itv $PWD:/workdir islasgeci/base R"
 alias run-dev='docker run --env BITBUCKET_USERNAME --env BITBUCKET_PASSWORD --interactive --name ${PWD##*/}_dev --rm --tty --volume ${PWD}:/workdir islasgeci/${PWD##*/}:develop bash'
 alias v=nvim
 alias vi=nvim
